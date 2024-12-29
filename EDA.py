a68d2c28ef1fa5807a0a686db5c3c1fffd8789b5
BroWang1 <brodeywang2004@gmail.com>
2024-12-29 01:07:10 -0500

    I was able to set up the data structure so know I can take look and get ready to find answers to my questions in the dataset

--- EDA.py
+++ EDA.py
@@ -1,12 +1,14 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
+import numpy as np
+
 file_paths = {r'/Users/thewang/Downloads/202021-hs-sqr-results.xlsx': '2020/21', r'/Users/thewang/Downloads/202122-hs-sqr-results.xlsx': '2021/22', r'/Users/thewang/Downloads/202223-hs-sqr-results.xlsx': '2022/23'}
 pd.set_option('display.max_rows', None)  # Display all rows
 pd.set_option('display.max_columns', None)  # Display all columns
 pd.set_option('display.width', None)  # Auto-detect the width
 pd.set_option('display.max_colwidth', None)  # No limit on column width
-
+pd.set_option('future.no_silent_downcasting', True)
 # The header skips the number of rows the next row will be your headers and if you are trying to use only certain columns
 #df = pd.read_excel(file_paths, header=3, usecols="D:CV")  # Syntax (file,header,usecols) which help analyze the excel at a certain position
 # This up here can only read one sheet at a time
@@ -23,14 +25,11 @@ for file, year in file_paths.items():   # accesing files and years from each fil
         year_data[sheet_name] = sheetdata   # stores the df of the files
     master_data_struc[year] = year_data
 
-
 all_file_sheets = set()
 for file, year in file_paths.items():   # This is to print all the sheet names in all of the excel sheets
     loading_excel = pd.ExcelFile(file)
     sheetname = loading_excel.sheet_names
     all_file_sheets.update(sheetname)
-
-
 print(all_file_sheets)  # List of sheets for all excel sheets
 
 data_2020 = master_data_struc['2020/21']["Summary"]
@@ -46,6 +45,21 @@ data_2020_Framework = master_data_struc['2020/21']["Framework"]
 data_2021_Framework = master_data_struc['2021/22']["Framework"]
 data_2022_Framework = master_data_struc['2022/23']["Framework"]
 
-print(list(data_2020_Additional_Info.columns))
-print(data_2020_Additional_Info.info)
+
+def convert_sheet_to_numeric_or_keep(df):
+    def convert_numeric_or_keep(col):
+        col = col.replace('N<15', np.nan)
+        col = col.replace('%', '', regex=True)
+        numeric_col = pd.to_numeric(col, errors='coerce')
+        return np.where(numeric_col.notna(), numeric_col, col)
+    return df.apply(lambda col: convert_numeric_or_keep(col))
+
+
+data_2020_Additional_Info = convert_sheet_to_numeric_or_keep(data_2020_Additional_Info)
+data_2021_Additional_Info = convert_sheet_to_numeric_or_keep(data_2021_Additional_Info)
+data_2022_Additional_Info = convert_sheet_to_numeric_or_keep(data_2022_Additional_Info)
+
+
+#print(list(data_2020_Additional_Info.columns))
+print(data_2020_Additional_Info.isnull().sum())
 print(data_2020_Additional_Info.describe())
