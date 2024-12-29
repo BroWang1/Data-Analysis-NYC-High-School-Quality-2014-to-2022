e14664592fb1ef811cdd433b0f5cd29687fef819
BroWang1 <brodeywang2004@gmail.com>
2024-12-29 00:13:57 -0500

    I was able to set up the data structure so know I can take look and get ready to find answers to my questions in the dataset

--- EDA.py
+++ EDA.py
@@ -16,7 +16,7 @@ for file, year in file_paths.items():   # accesing files and years from each fil
     loading_excel = pd.ExcelFile(file)  # This is so you can read multiple files and sheets
     year_data = {}  # In general initializing allows different type of data to be stored to be manipulated without being deleted
     for sheet_name in loading_excel.sheet_names:
-        checking_cols = pd.read_excel(file, sheet_name=sheet_name, header=3, nrows=10)     # This is to get all the columns that arent empty
+        checking_cols = pd.read_excel(file, sheet_name=sheet_name, header=3, nrows=600)     # This is to get all the columns that arent empty
         cols = checking_cols.dropna(axis=1, how='all').columns
         usecols = cols.tolist()     # Put this into a lost so no errors
         sheetdata = pd.read_excel(loading_excel, sheet_name=sheet_name, header=3, usecols=usecols)
@@ -33,30 +33,19 @@ for file, year in file_paths.items():   # This is to print all the sheet names i
 
 print(all_file_sheets)  # List of sheets for all excel sheets
 
-data_2020 = master_data_struc['2020/21']["Summary"].head()
-data_2021 = master_data_struc['2021/22']["Summary"].head()
-data_2022 = master_data_struc['2022/23']["Summary"].head()
-data_2020_Student = master_data_struc['2020/21']["Student Achievement"].head()
-data_2021_Student = master_data_struc['2021/22']["Student Achievement"].head()
-data_2022_Student = master_data_struc['2022/23']["Student Achievement"].head()
-data_2020_Additional_Info = master_data_struc['2020/21']["Additional Info"].head()
-data_2021_Additional_Info = master_data_struc['2021/22']["Additional Info"].head()
-data_2022_Additional_Info = master_data_struc['2022/23']["Additional Info"].head()
-data_2020_Framework = master_data_struc['2020/21']["Framework"].head()
-data_2021_Framework = master_data_struc['2021/22']["Framework"].head()
-data_2022_Framework = master_data_struc['2022/23']["Framework"].head()
+data_2020 = master_data_struc['2020/21']["Summary"]
+data_2021 = master_data_struc['2021/22']["Summary"]
+data_2022 = master_data_struc['2022/23']["Summary"]
+data_2020_Student = master_data_struc['2020/21']["Student Achievement"]
+data_2021_Student = master_data_struc['2021/22']["Student Achievement"]
+data_2022_Student = master_data_struc['2022/23']["Student Achievement"]
+data_2020_Additional_Info = master_data_struc['2020/21']["Additional Info"]
+data_2021_Additional_Info = master_data_struc['2021/22']["Additional Info"]
+data_2022_Additional_Info = master_data_struc['2022/23']["Additional Info"]
+data_2020_Framework = master_data_struc['2020/21']["Framework"]
+data_2021_Framework = master_data_struc['2021/22']["Framework"]
+data_2022_Framework = master_data_struc['2022/23']["Framework"]
 
-#print(list(data_2020_Additional_Info.columns))
-
-data_2020_Additional_Info.loc[:, 'Metric Value - Average score of students in the current cohort who took the SAT Reading and Writing exam'] = pd.to_numeric(
-    data_2020_Additional_Info['Metric Value - Average score of students in the current cohort who took the SAT Reading and Writing exam'],
-    errors='coerce'
-)
-
-
-data_2020_Additional_Info['Metric Value - Average score of students in the current cohort who took the SAT Math exam'] = pd.to_numeric(data_2020_Additional_Info['Metric Value - Average score of students in the current cohort who took the SAT Math exam'], errors='coerce')
-data_2020_Additional_Info['Metric Value - Average score of students in the current cohort who took the SAT Reading and Writing exam'] = pd.to_numeric(data_2020_Additional_Info['Metric Value - Average score of students in the current cohort who took the SAT Reading and Writing exam'], errors='coerce')
-
-print(data_2020_Additional_Info.dtypes)
-print(data_2020_Additional_Info['Metric Value - Average score of students in the current cohort who took the SAT Reading and Writing exam'].describe())
-print(data_2020_Additional_Info['Metric Value - Average score of students in the current cohort who took the SAT Math exam'].describe())
+print(list(data_2020_Additional_Info.columns))
+print(data_2020_Additional_Info.info)
+print(data_2020_Additional_Info.describe())
